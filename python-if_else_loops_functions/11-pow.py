352cb554b21e513aa77b7bc1150cbb8a104577b5
Gabriel <acostagabriel1311@gmai.com>
2023-05-30 12:58:42 -0700

    task 11

---
+++ python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def pow(a, b):
+    if b >= 0:
+        result = 1
+        for _ in range(b):
+            result *= a
+    else:
+        result = 1
+        for _ in range(abs(b)):
+            result /= a
+    return result
