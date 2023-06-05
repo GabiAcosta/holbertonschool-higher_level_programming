f399d82198c637edf0aafa66fb3f914fa24af208
Gabriel <acostagabriel1311@gmai.com>
2023-06-05 10:30:18 -0700

    different way to do the task, same outputs with the given main

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,8 +2,7 @@
 def no_c(my_string):
     if my_string:
         new_string = ""
-        for i in range(len(my_string)):
-            if my_string[i] == 'c' or my_string[i] == 'C':
-                continue
-            new_string += my_string[i]
+        for i in my_string:
+            if i not in "cC":
+                new_string += i
         return new_string
