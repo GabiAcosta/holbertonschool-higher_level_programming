d62e29336ec54baaf48e95a1b8d8c83c47a259fd
Gabriel <acostagabriel1311@gmai.com>
2023-06-05 09:22:10 -0700

    possible fix, identation problem

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -6,4 +6,4 @@ def no_c(my_string):
             if my_string[i] == 'c' or my_string[i] == 'C':
                 continue
             new_string += my_string[i]
-    return new_string
+        return new_string
