9b14f1116cad0fbda5971862e23f9f59dec1bb48
Gabriel <acostagabriel1311@gmai.com>
2023-06-06 13:19:39 -0700

    nicer code :)

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,10 +2,7 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return
-    max_value = 0
-    for k, v in a_dictionary.items():
-        if max_value < v:
-            max_value = v
+    max_value = max(a_dictionary.values())
     for k, v in a_dictionary.items():
         if max_value == v:
             return k
