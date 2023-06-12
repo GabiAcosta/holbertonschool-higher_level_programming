d15f90f36ed24da2a29c6420a353c51f6463dc8f
Gabriel <acostagabriel1311@gmai.com>
2023-06-12 09:19:38 -0700

    task 0 and README

---
+++ python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class Square:
+    """Empty class that defines a square.
+
+    Attributes:
+        No attributes currently.
+
+    Methods:
+        No methods currently.
+    """
+    pass
