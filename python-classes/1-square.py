9275e0df435296acbde3d13c84678c5b708449c3
Gabriel <acostagabriel1311@gmai.com>
2023-06-12 09:41:08 -0700

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Task 1 """
+
+
+class Square:
+    def __init__(self, size):
+        """
+        Initializes a Square object.
+
+        Args:
+            size: The size of the square.
+
+        Returns:
+            None.
+        """
+        self.__size = size
