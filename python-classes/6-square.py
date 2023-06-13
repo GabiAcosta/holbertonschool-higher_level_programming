073ec88875c8194fecd11d4f2608d1314035bdfd
Gabriel <acostagabriel1311@gmai.com>
2023-06-13 08:52:21 -0700

    task 6 and modified minor error on doc in task 5

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
-""" Task 5 """
+""" Task 6 """
 
 
 class Square:
     """ Class that defines a square """
 
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         """
         Initializes a Square object.
 
         Args:
             size (int): The size of the square.
+            position (tuple): The position of the square.
 
         Returns:
             None.
         """
         self.size = size
+        self.position = position
 
     @property
     def size(self):
@@ -50,6 +52,35 @@ class Square:
         else:
             raise TypeError("size must be an integer")
 
+    @property
+    def position(self):
+        """
+        Getter method for the position attribute.
+
+        Returns:
+            tuple: The current position of the square.
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """
+        Setter method for the position attribute.
+
+        Args:
+            value (tuple): The position value to be set.
+
+        Raises:
+            TypeError: If position is not a tuple of 2 positive integers.
+
+        Returns:
+            None.
+        """
+        if not check_positive_tuple(value) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
     def area(self):
         """
         Calculates the area of the square.
@@ -69,7 +100,30 @@ class Square:
         """
         if self.size == 0:
             print("")
+        if self.position[1] > 0:
+            for _ in range(self.position[1]):
+                print()
         for _ in range(self.size):
+            for _ in range(self.position[0]):
+                print(" ", end="")
             for _ in range(self.size):
                 print("#", end="")
             print()
+
+
+def check_positive_tuple(value):
+    """
+    Checks if a tuple contains positive integers.
+
+    Args:
+        value (tuple): The tuple to be checked.
+
+    Returns:
+        bool: True if the tuple contains positive integers, False otherwise.
+    """
+    if not isinstance(value, tuple):
+        return False
+    for elem in value:
+        if not isinstance(elem, int) or elem < 0:
+            return False
+    return True
