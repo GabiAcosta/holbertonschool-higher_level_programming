20e424c05082c7b4f075ac3d54596fa08b7206f6
GabiAcosta <acostagabriel1311@gmail.com>
2023-06-14 12:19:21 -0700

    task 3

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,21 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
+"""Task 3"""
+
+
 def say_my_name(first_name, last_name=""):
+    """
+    Prints the name with the provided first name and last name.
+
+    Args:
+        first_name (str): The first name.
+        last_name (str): The last name. Default is an empty string.
+
+    Raises:
+        TypeError: If first_name or last_name is not a string.
+
+    Returns:
+        None.
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     elif not isinstance(last_name, str):
