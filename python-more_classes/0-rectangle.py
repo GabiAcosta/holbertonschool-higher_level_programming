8781e26c1c074ee6382e9804c807908af9e2d313
Gabriel <acostagabriel1311@gmai.com>
2023-06-15 08:43:53 -0700

    task 0 and README

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Task 0"""
+
+
+class Rectangle:
+    """Empty class that defines a rectangle.
+
+    Attributes:
+        No attributes currently.
+
+    Methods:
+        No methods currently.
+    """
+    pass
