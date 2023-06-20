c857cd6a6751feeecac634e9c89bcf1ef0384aab
Gabriel <acostagabriel1311@gmai.com>
2023-06-20 14:53:38 -0700

    task 8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Task 8"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Class representing a rectangle.
+
+    Attributes:
+        __width (int): The width of the rectangle.
+        __height (int): The height of the rectangle.
+
+    Methods:
+        __init__(width, height): Initializes a rectangle with the given width
+        and height.
+
+    """
+    def __init__(self, width, height):
+        """
+        Initializes a rectangle with the given width and height.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+
+        Returns:
+            None
+
+        """
+        if Rectangle.integer_validator(self, "width", width):
+            self.__width = width
+        if Rectangle.integer_validator(self, "heigth", height):
+            self.__height = height
