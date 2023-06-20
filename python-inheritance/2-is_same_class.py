c59367a3e3194e2f12e3d1e362dc4a5f87ebcf4a
Gabriel <acostagabriel1311@gmai.com>
2023-06-20 09:14:28 -0700

    task 2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Task 2"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Checks if an object is an instance of a specified class.
+
+    Args:
+        obj: The object to be checked.
+        a_class: The class to compare against.
+
+    Returns:
+        bool: True if the object is an instance of the class, False otherwise.
+
+    """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
