39603b9443a39c59c61eaa4fc3ed09319fd4bd9e
Gabriel <acostagabriel1311@gmai.com>
2023-06-20 12:47:48 -0700

    task 5

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Task 5"""
+
+
+class BaseGeometry:
+    """
+    Base class for geometry-related operations.
+
+    Attributes:
+        None
+
+    Methods:
+        None
+
+    """
+    pass
