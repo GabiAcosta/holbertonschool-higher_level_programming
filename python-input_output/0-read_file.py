41e0ae5a186117282962d5ba8cd97bc530525980
Gabriel <acostagabriel1311@gmai.com>
2023-06-22 08:09:21 -0700

    changed the print

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -16,4 +16,4 @@ def read_file(filename=""):
     """
     with open(filename, encoding="utf-8") as f:
         text = f.read()
-        print("{}".format(text))
+        print("{}".format(text), end="")
