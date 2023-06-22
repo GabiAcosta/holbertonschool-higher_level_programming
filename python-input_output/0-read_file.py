281c5603be9733d4bb9a38c2da29cbd9523f0a7c
Gabriel <acostagabriel1311@gmai.com>
2023-06-22 08:01:59 -0700

    task 0 and README

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Task 0"""
+
+
+def read_file(filename=""):
+    """
+    Read and print the contents of a file.
+
+    Args:
+        filename (str): The name of the file to be read. Defaults to an
+        empty string.
+
+    Returns:
+        None
+
+    """
+    with open(filename, encoding="utf-8") as f:
+        text = f.read()
+        print(text)
