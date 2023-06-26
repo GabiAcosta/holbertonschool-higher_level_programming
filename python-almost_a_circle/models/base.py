be6d72aa96a0bd7ba019711656afb4bee71ce03e
Gabriel <acostagabriel1311@gmai.com>
2023-06-26 08:10:56 -0700

    base class

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Defines a Base model class"""
+
+
+class Base:
+    """
+    Base model class.
+
+    Attributes:
+        __nb_objects (int): Counter for the number of instances of Base.
+
+    Methods:
+        __init__(self, id=None): Initializes a Base object.
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Initializes a Base object.
+
+        Args:
+            id (int): The ID of the object. If not provided, the ID is
+            automatically assigned based on the current number of instances.
+
+        Returns:
+            None.
+        """
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
