d93c1b060bed47339261d0ae8feba062b7cb85e3
Gabriel <acostagabriel1311@gmai.com>
2023-06-26 11:38:51 -0700

    added magic method str

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -122,3 +122,8 @@ class Rectangle(Base):
             for _ in range(self.__width):
                 print("#", end="")
             print()
+
+    def __str__(self):
+        return "[{}] ({}) {}/{} - {}/{}".format(
+            self.__class__.__name__, self.id, self.__x,
+            self.__y, self.__width, self.__height)
