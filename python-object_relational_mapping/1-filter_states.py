e2329f0895257c5af6e7f79694b51b6dd4582f55
Gabriel <acostagabriel1311@gmai.com>
2023-07-27 14:22:21 -0700

    goofy fix for pycodestyle

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -9,7 +9,10 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id")
+    que = "SELECT * FROM states WHERE name "
+    ry = "LIKE BINARY 'N%' ORDER BY states.id"
+    query = que + ry
+    cur.execute(query)
     fetchs = cur.fetchall()
     for i in fetchs:
         print(i)
